750489417216e11a716736be1ff3adfb3a892fce
AlexandreZenas <94380403+AlexandreZenas@users.noreply.github.com>
2023-02-11 05:06:50 -0300

    'commitChanges'

--- js/App.js
+++ js/App.js
@@ -1,6 +1,12 @@
+//array Pages
   const allContainers = document.querySelectorAll('.Pages');
-  const nodeArray = Array.from(allContainers);
- 
+  const nodeArray = Array.from(allContainers); 
+//array Spans
+  const allSpan = document.querySelectorAll('.containerConteudo > span');
+  const spanArray = Array.from(allSpan)
+//Array Li
+  const navMenuLi = document.querySelectorAll('.NumbersOl > li');
+  const liArray = Array.from(navMenuLi)
 
   for(let i = 0; i < nodeArray.length; i++){
 
@@ -8,37 +14,48 @@
         var y = event.deltaY;
 
         if( y > 0 && i < 4 ){     
-          
-          // nodeArray[i].style.zIndex = 6;
           //adiciona e remove as classes posteriores
-            nodeArray[i+1].classList.add('visibilityPage');
-            nodeArray[i+1].classList.remove('animationPageUp');
+          liArray[i+1].classList.add('activeSlide')
+          spanArray[i+1].classList.remove('animationSpan');         
+          nodeArray[i+1].classList.add('visibilityPage');
+          nodeArray[i+1].classList.remove('animationPageUp');
+
+          /*esse timeOut serve para que o 'animateSpan' seja removido e adicionado novamente, para a animation
+          funcionar*/
+          setTimeout(() => {
+            spanArray[i+1].classList.add('animationSpan');  
+          },100); 
 
           //adiciona e remove as classes atuais
-            nodeArray[i].classList.remove('visibilityPage');
-            nodeArray[i].classList.add('animationPageUp');
-          //adiciona e remove as classes anteriores
-            nodeArray[i-1].classList.remove('animationPageUp');
-            // nodeArray[i-1].style.zIndex = 5;
-            setTimeout(() => {
-              // nodeArray[i].style.zIndex = 5;
-              // nodeArray[i].style.zIndex = 5;
-            }, 800);
-            
-        }else if (y < 0 && i <= 4){
-          // nodeArray[i].style.zIndex = 6;
-        //remove as classes anteriores
-          nodeArray[i-1].classList.add('visibilityPage');
-          nodeArray[i-1].classList.remove('animationPageUp');
-        //remove as classes atuais
+          liArray[i].classList.remove('activeSlide')
           nodeArray[i].classList.remove('visibilityPage');
           nodeArray[i].classList.add('animationPageUp');
-        //remove as classes posteriores
-          // nodeArray[i-1].style.zIndex = 5;
-            setTimeout(() => {
 
-            }, 800);
+          //remove as classes anteriores
+          nodeArray[i-1].classList.remove('animationPageUp');   
+        }
+        else if (y < 0 && i <= 4){
+          //adiciona e remove as classes anteriores
+          liArray[i-1].classList.add('activeSlide')
+          spanArray[i-1].classList.remove('animationSpan');
+          nodeArray[i-1].classList.add('visibilityPage');
+          nodeArray[i-1].classList.remove('animationPageUp');
 
-        }   
+          //timeOut para a animation do Span
+          setTimeout(() => {
+            spanArray[i-1].classList.add('animationSpan');
+          },100); 
+
+          //adiciona e remove as classes atuais
+          nodeArray[i].classList.remove('visibilityPage');
+          nodeArray[i].classList.add('animationPageUp');
+          liArray[i].classList.remove('activeSlide');
+
+          //remove as classes posteriores
+          nodeArray[i+1].classList.remove('animationPageUp');        
+        }
+    });
+    liArray[i].addEventListener('click', (event) => {
+      console.log(liArray[i])
     });
   }
